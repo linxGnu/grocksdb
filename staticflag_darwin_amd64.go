d5376bccf6f2425df8ab427c474deae948244207
linxGnu <linhduonggnu@gmail.com>
2019-10-16 00:52:22 +0900

    Support darwin build

---
+++ staticflag_darwin_amd64.go
@@ -0,0 +1,18 @@
+// +build static
+
+package grocksdb
+
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/bz2
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/lz4
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/snappy
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/zlib
+// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/zstd
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/bz2
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/lz4
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/snappy
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/zlib
+// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include/zstd
+// #cgo LDFLAGS: -L ${SRCDIR}/dist/darwin_amd64 -lz -llz4 -lzstd -lsnappy -lrocksdb -lm -ldl -lstdc++
+import "C"
