dfaa7b4645075f8e9a59fe6d51ba8e2603bd57a0
linxGnu <linhduonggnu@gmail.com>
2019-10-21 00:41:04 +0900

    Built with: gcc (GCC) 8.2.1 20180905 (Red Hat 8.2.1-3)

--- build_centos.sh
+++ build_centos.sh
@@ -3,32 +3,14 @@
 # update sys
 yum update -y
 
-# install gcc-8
-yum install -y centos-release-scl
-yum install -y devtoolset-8-gcc devtoolset-8-gcc-c++
-scl enable devtoolset-8 -- bash
-
 # install tool
-yum install -y which git
+yum install -y gcc gcc-c++ git pkg-config make which
 
-# install deps
-yum install -y snappy-devel
-
-# install cmake
-curl https://cmake.org/files/v3.11/cmake-3.11.4.tar.gz -o cmake-3.11.4.tar.gz
-tar xzf cmake-3.11.4.tar.gz
-pushd cmake-3.11.4
-./bootstrap --prefix=/opt/cmake --no-system-libs --parallel=16
-make install -j16
-popd
-rm -rf cmake-3.11.4 cmake-3.11.4.tar.gz
+# install deps (for linking) and temporary build tool
+yum install -y snappy-devel cmake
 
 # build rocksdb
-PATH=$PATH:/opt/cmake/bin
 make
 
-# remove snappy-devel
-yum remove -y snappy-devel
-
-# remove cmake
-rm -rf /opt/cmake
+# remove deps
+yum remove -y snappy-devel cmake
