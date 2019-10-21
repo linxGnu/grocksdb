a089a768b875971bb3825d89e80261a857a495a3
linxGnu <linhduonggnu@gmail.com>
2019-10-21 13:30:21 +0900

    Update build script/tags

--- staticflag_darwin_amd64.go
+++
@@ -1,8 +0,0 @@
-// +build static_rocksdb
-
-package grocksdb
-
-// #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
-// #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
-// #cgo LDFLAGS: -L ${SRCDIR}/dist/darwin_amd64/lib -lrocksdb -lstdc++ -lm -ldl -lzstd -llz4 -lz -lsnappy -lbz2
-import "C"
--- staticflag_linux_amd64.go
+++
@@ -1,8 +0,0 @@
-// +build static_rocksdb
-
-package grocksdb
-
-// #cgo CFLAGS: -I ${SRCDIR}/dist/linux_amd64/include
-// #cgo CXXFLAGS: -I ${SRCDIR}/dist/linux_amd64/include
-// #cgo LDFLAGS: -L ${SRCDIR}/dist/linux_amd64/lib -lrocksdb -lstdc++ -lm -ldl -lzstd -llz4 -lz -lsnappy -lbz2
-import "C"
