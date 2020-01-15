3575b46e202e6d2700d898dbcd68438715b8d923
linxGnu <linhduonggnu@gmail.com>
2020-01-15 16:44:29 +0900

    Link against pthread

--- builtin_static_linux_amd64.go
+++ builtin_static_linux_amd64.go
@@ -4,5 +4,5 @@ package grocksdb
 
 // #cgo CFLAGS: -I ${SRCDIR}/dist/linux_amd64/include -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free
 // #cgo CXXFLAGS: -I ${SRCDIR}/dist/linux_amd64/include -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free
-// #cgo LDFLAGS: -L ${SRCDIR}/dist/linux_amd64/lib -lrocksdb -lstdc++ -lm -ldl -lzstd -llz4 -lz -lsnappy -lbz2 -ljemalloc_pic -pthread
+// #cgo LDFLAGS: -L ${SRCDIR}/dist/linux_amd64/lib -lpthread -lrocksdb -lstdc++ -lm -ldl -lzstd -llz4 -lz -lsnappy -lbz2 -ljemalloc_pic
 import "C"
