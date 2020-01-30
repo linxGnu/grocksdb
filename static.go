a3e308bd8def98aa1b62bda7b27060b84f7c4fdd
linxGnu <linhduonggnu@gmail.com>
2020-01-30 22:13:19 +0900

    Try2

--- static.go
+++ static.go
@@ -2,5 +2,5 @@
 
 package grocksdb
 
-// #cgo LDFLAGS: -lrocksdb -pthread -ldl -lm -lzstd -llz4 -lz -lsnappy
+// #cgo LDFLAGS: -lrocksdb -pthread -lstdc++ -ldl -lm -lzstd -llz4 -lz -lsnappy
 import "C"
