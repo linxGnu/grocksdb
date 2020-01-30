6055476659af16fc7d639cf12977e726b9feb856
linxGnu <linhduonggnu@gmail.com>
2020-01-30 21:38:37 +0900

    Trying

--- builtin_static_darwin_amd64.go
+++ builtin_static_darwin_amd64.go
@@ -4,5 +4,5 @@ package grocksdb
 
 // #cgo CFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
 // #cgo CXXFLAGS: -I ${SRCDIR}/dist/darwin_amd64/include
-// #cgo LDFLAGS: -L${SRCDIR}/dist/darwin_amd64/lib -L${SRCDIR}/dist/darwin_amd64/lib64 -lrocksdb -pthread -lstdc++ -ldl -lm -lzstd -llz4 -lz -lsnappy
+// #cgo LDFLAGS: -L${SRCDIR}/dist/darwin_amd64/lib -lrocksdb -pthread -ldl -lm -lzstd -llz4 -lz -lsnappy
 import "C"
