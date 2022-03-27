ee0c6690507f212939bfc33ff99d7ce26af0789a
linxGnu <linhduonggnu@gmail.com>
2022-03-27 11:06:12 +0900

    Adapt rocksdb 7.0.3

--- filter_policy_test.go
+++ filter_policy_test.go
@@ -5,22 +5,22 @@ import (
 )
 
 func TestFilterPolicy(t *testing.T) {
-	t.Run("Bloom", func(t *testing.T) {
+	t.Run("Bloom", func(*testing.T) {
 		flt := NewBloomFilter(1.2)
 		defer flt.Destroy()
 	})
 
-	t.Run("BloomFull", func(t *testing.T) {
+	t.Run("BloomFull", func(*testing.T) {
 		flt := NewBloomFilterFull(1.2)
 		defer flt.Destroy()
 	})
 
-	t.Run("Ribbon", func(t *testing.T) {
+	t.Run("Ribbon", func(*testing.T) {
 		flt := NewRibbonFilterPolicy(1.2)
 		defer flt.Destroy()
 	})
 
-	t.Run("RibbonHybrid", func(t *testing.T) {
+	t.Run("RibbonHybrid", func(*testing.T) {
 		flt := NewRibbonHybridFilterPolicy(1.2, 1)
 		defer flt.Destroy()
 	})
